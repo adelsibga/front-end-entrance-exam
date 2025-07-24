5781e475e4c74b939009c1a77db9b6b77331e897
Adel Sibgatullin <adelsibga@gmail.com>
2025-07-25 01:21:10 +0300

    master Правки для меню

--- scripts/components/menu/index.js
+++ scripts/components/menu/index.js
@@ -1,7 +1,11 @@
-import './style.scss'
+import './style.scss';
 
 export function drawMenu() {
   return `
-    <button id="save-pdf">Save PDF</button>
-  `
+    <header class='menu'>
+      <div class='menu__wrapper'>
+        <button class='menu__button' id='save-pdf'>Save PDF</button>
+      </div>
+    </header>
+  `;
 }
